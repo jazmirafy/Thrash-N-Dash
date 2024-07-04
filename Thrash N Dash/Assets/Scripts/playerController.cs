db12c588c112cad37d86896fb08b27d37f4f2cc9
jazmirafy <egyptjazmean@gmail.com>
2024-07-03 22:04:07 -0400

    Added Jump
    Player controller script now includes jumping

--- Thrash N Dash/Assets/Scripts/playerController.cs
+++ Thrash N Dash/Assets/Scripts/playerController.cs
@@ -10,6 +10,13 @@ public class playerController : MonoBehaviour
     Animator myAnim;
     bool facingRight;
 
+    //jumping variables
+    bool grounded = false;
+    float groundCheckerRadius = 0.5f;
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    public float jumpHeight;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,8 +27,20 @@ public class playerController : MonoBehaviour
     }
 
     // Update is called once per frame
+    void Update(){
+        if(grounded && Input.GetAxis("Jump")>0){
+            myAnim.SetBool("isGrounded", grounded);
+            myRB.AddForce(new Vector2(0, jumpHeight));
+        }
+    }
     void FixedUpdate()
     {
+        //check if player is on the ground (otherwise the player is falling)
+        //if player is intersecting the ground "grounded" will be true, otherwise, false.
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckerRadius, groundLayer);
+        myAnim.SetBool("isGrounded", grounded);
+        myAnim.SetFloat("VerticleSpeed", myRB.velocity.y);
+
         float move = Input.GetAxis("Horizontal");
         myRB.velocity = new Vector2(move * maxSpeed, myRB.velocity.y);
 
@@ -31,7 +50,7 @@ public class playerController : MonoBehaviour
             Flip();
         }
     }
-
+    //this flips the character to the right orientation when they switch the direction they are moving
     void Flip(){
         facingRight = !facingRight;
         Vector3 theScale = transform.localScale;

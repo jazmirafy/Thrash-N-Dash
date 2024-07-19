64b147c6a0b7747ecec67c9dae9450175ef33cef
jazmirafy <egyptjazmean@gmail.com>
2024-07-19 14:32:56 -0500

    reposition camera + jump controller script

--- Thrash N Dash/Assets/Scripts/JumpController.cs
+++ Thrash N Dash/Assets/Scripts/JumpController.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class JumpController : MonoBehaviour
 {
+    public Transform landingPosition; // Set this to the desired landing position in the inspector
+    public float jumpHeight = 5f; // Maximum height of the jump
+    public float jumpDuration = 1f; // Duration of the jump from start to finish
+
+    private Vector2 startPosition;
+    private Vector2 targetPosition;
+    private bool isJumping = false;
+    private float jumpProgress = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
+    public void StartJump( Vector2 landingPos)
+    {
+        //landingPositon = gameObject.GetComponent<Transform>();
+
+
+        startPosition = transform.position;
+        targetPosition = landingPos;
+        isJumping = true;
+        jumpProgress = 0f;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // If currently jumping, update the jump
+        if (isJumping)
+        {
+            UpdateJump();
+        }
+    }
+        void UpdateJump()
+    {
+        jumpProgress += Time.deltaTime / jumpDuration;
+        if (jumpProgress >= 1f)
+        {
+            jumpProgress = 1f;
+            isJumping = false;
+        }
+
+        // Calculate the position using a parabolic equation
+        float height = Mathf.Sin(Mathf.PI * jumpProgress) * jumpHeight;
+        Vector2 currentPosition = Vector2.Lerp(startPosition, targetPosition, jumpProgress);
+        currentPosition.y += height;
+
+        transform.position = currentPosition;
     }
 }
